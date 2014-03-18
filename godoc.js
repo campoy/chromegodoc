6733c6fa42b45157084da0862fa53049cddb2afa
Francesc Campoy <campoy@golang.org>
2014-03-18 01:06:59 -0700

    improved handling of github urls

--- godoc.js
+++ godoc.js
@@ -17,7 +17,7 @@ chrome.runtime.onInstalled.addListener(function() {
                     pageUrl: {
                         hostEquals: 'code.google.com'
                     },
-                })
+                }),
                 new chrome.declarativeContent.PageStateMatcher({
                     pageUrl: {
                         hostEquals: 'launchpad.net'
@@ -27,7 +27,7 @@ chrome.runtime.onInstalled.addListener(function() {
                     pageUrl: {
                         hostEquals: 'bitbucket.org'
                     },
-                }),
+                })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
@@ -35,12 +35,32 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-    console.log(tab);
     var a = document.createElement('a');
     a.href = tab.url;
     chrome.tabs.create({
-        url: 'http://godoc.org/' + a.hostname + a.pathname,
+        url: 'http://godoc.org/' + godocPath(a.hostname, a.pathname),
         active: true,
         index: tab.index + 1,
     });
 });
+
+var godocPath = function(host, path) {
+    if (host == 'github.com') {
+        var pieces = path.substr(1).split('/');
+        if (pieces.length > 4 && pieces[3] == "master" && pieces[2] == "tree") {
+            // if we see tree/master just ignore that part
+            pieces = pieces.slice(0, 2).concat(pieces.splice(4));
+        } else if (pieces.length > 4 && pieces[3] == "master" && pieces[2] == "blob") {
+            // but if we find a blob, we ignore also the filename
+            pieces = pieces.slice(0, 2).concat(pieces.splice(4, pieces.length - 5));
+        }
+        return host + '/' + pieces.join('/')
+    }
+
+    if (host == 'code.google.com') {
+        return host + path.split('/').slice(0, 3).join('/');
+    }
+
+    // non github
+    return host + path;
+};
